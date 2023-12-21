5fb24cbbe3dc829cf9d425c7fd0872bf856e080c
Nikita Sivukhin <sivukhin@sharechat.co>
2023-12-21 23:30:40 +0400

    wip

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"go/ast"
+	"go/token"
 	"log"
 	"os"
 	"os/exec"
@@ -18,27 +19,127 @@ import (
 type E struct{ Desc string }
 
 func (e *E) Error() string { return e.Desc }
-func api() *E              { return nil }
-
-func use() {
-	var err error = api()
-	if err == nil {
-		panic("this is impossible")
+func api(n int) *E {
+	if n == 0 {
+		return nil
 	}
+	return &E{Desc: "error"}
 }
 
-func checkSubtreeComplexity(node ast.Node) bool {
+func use(n int) {
+	var err error = api(n)
+	if err != nil {
+		panic("this is impossible")
+	}
+	panic(fmt.Sprintf("this is possible: %v", err))
+}
+
+var simpleBuiltins = []string{
+	"make",
+	"cap",
+	"len",
+	"complex",
+	"imag",
+	"max",
+	"min",
+	"real",
+	"bool",
+	"byte",
+	"string",
+	"complex128",
+	"complex64",
+	"float32",
+	"float64",
+	"int",
+	"int16",
+	"int32",
+	"int64",
+	"int8",
+	"rune",
+	"uint",
+	"uint16",
+	"uint32",
+	"uint64",
+	"uint8",
+	"uintptr",
+}
+
+func checkNodeComplexity(node ast.Node) bool {
 	isComplex := false
 	ast.Inspect(node, func(node ast.Node) bool {
-		switch node.(type) {
+		switch n := node.(type) {
+		case *ast.ReturnStmt, *ast.ForStmt, *ast.RangeStmt, *ast.DeferStmt:
+			isComplex = true
 		case *ast.CallExpr:
-			isComplex = true
-		case *ast.ReturnStmt:
-			isComplex = true
+			if ident, ok := n.Fun.(*ast.Ident); ok {
+				isComplex = isComplex || !slices.Contains(simpleBuiltins, ident.Name)
+			} else {
+				isComplex = false
+			}
+		case *ast.BinaryExpr:
+			isComplex = isComplex || n.Op == token.ARROW
 		}
 		return true
 	})
 	return isComplex
+}
+
+func reportIfVanished(
+	fset *token.FileSet,
+	assemblyLines map[string][]int,
+	currentFunc string,
+	start, end token.Pos,
+) bool {
+	startPosition, endPosition := fset.Position(start), fset.Position(end)
+	lines, ok := assemblyLines[startPosition.Filename]
+	if !ok {
+		return false
+	}
+	index, _ := slices.BinarySearch(lines, startPosition.Line)
+	if index < len(lines) && lines[index] <= endPosition.Line {
+		return false
+	}
+	log.Printf("it seems like your code vanished from compiled binary: func=[%v], file=[%v], line=[%v]", currentFunc, startPosition.Filename, startPosition.Line)
+	return true
+}
+
+func isGenericFunc(funcDecl *ast.FuncDecl) bool {
+	if funcDecl.Type.TypeParams != nil {
+		return true
+	}
+	if funcDecl.Recv != nil {
+		generic := false
+		for _, recv := range funcDecl.Recv.List {
+			target := recv.Type
+			for {
+				if star, ok := target.(*ast.StarExpr); ok {
+					target = star.X
+				} else {
+					break
+				}
+			}
+			if _, ok := target.(*ast.IndexListExpr); ok {
+				generic = true
+			}
+			if _, ok := target.(*ast.IndexExpr); ok {
+				generic = true
+			}
+		}
+		return generic
+	}
+	return false
+}
+
+func isPivot(node ast.Node) (body *ast.BlockStmt, ok bool) {
+	switch n := node.(type) {
+	case *ast.IfStmt:
+		return n.Body, true
+	case *ast.ForStmt:
+		return n.Body, true
+	case *ast.RangeStmt:
+		return n.Body, true
+	}
+	return nil, false
 }
 
 func main() {
@@ -107,30 +208,44 @@ func main() {
 				continue
 			}
 			var currentFunc string
-			ast.Inspect(file, func(node ast.Node) bool {
+			var analyze func(node ast.Node) bool
+
+			analyze = func(node ast.Node) bool {
 				if funcDecl, ok := node.(*ast.FuncDecl); ok {
+					if isGenericFunc(funcDecl) {
+						return false
+					}
 					currentFunc = funcDecl.Name.Name
 					return true
 				}
-				_, ok := node.(*ast.IfStmt)
+				if _, pivot := isPivot(node); pivot {
+					return true
+				}
+				blockStmt, ok := node.(*ast.BlockStmt)
 				if !ok {
 					return true
 				}
-				if !checkSubtreeComplexity(node) {
-					return false
-				}
-				start, end := pkg.Fset.Position(node.Pos()), pkg.Fset.Position(node.End())
-				startLine, endLine := start.Line, end.Line
-				lines, ok := assemblyLines[start.Filename]
-				if !ok {
-					return true
-				}
-				position, _ := slices.BinarySearch(lines, startLine)
-				if position >= len(lines) || lines[position] > endLine {
-					log.Printf("it seems like your code vanished from compiled binary: func=[%v], file=[%v], line=[%v]", currentFunc, start.Filename, start.Line)
+				previous := -1
+				for i := 0; i <= len(blockStmt.List); i++ {
+					pivot := false
+					if i == len(blockStmt.List) {
+						pivot = true
+					} else {
+						switch blockStmt.List[i].(type) {
+						case *ast.IfStmt, *ast.ForStmt, *ast.RangeStmt, *ast.BlockStmt:
+							pivot = true
+						}
+					}
+					if pivot {
+						if checkNodeComplexity(&ast.BlockStmt{List: blockStmt.List[previous+1 : i]}) {
+							reportIfVanished(pkg.Fset, assemblyLines, currentFunc, blockStmt.List[previous+1].Pos(), blockStmt.List[i-1].End())
+						}
+						previous = i
+					}
 				}
 				return true
-			})
+			}
+			ast.Inspect(file, analyze)
 		}
 	}
 }
